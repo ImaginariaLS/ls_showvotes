2619be5412c5380c30fc1f54cb1ccdd2db8490e2
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-03 23:24:48 +0300

    refactoring

--- config/config.php
+++ config/config.php
@@ -13,29 +13,35 @@ $config = array();
  * "all" or any other value — all users including guests
  */
 $config['can_see'] = "all";
+
 /**
  * positive voters display limit
  * -1 = no limit
  */
 $config['positive_display_limit'] = 20;
+
 /**
  * negative voters display limit
  * -1 = no limit
  */
 $config['negative_display_limit'] = 20;
+
 /**
  * abstaining voters display limit
  * -1 = no limit
  */
 $config['neutral_display_limit'] = 20;
+
 /**
  * Enables topic_add date restriction.
- */ 
+ */
 $config['topic_add_date_restriction'] = false;
+
 /**
  * Voters will be displayed only in topics which date_add will be later than this value.
  * To disable it set $config['topic_add_date_restriction'] to false;
  */
 $config['available_from_date'] = '2014-10-19 00:00:00';
+
+
 return $config;
-?>
